f89f38d5e8433ab24d0fdc3631de0e1be42bf8e1
Muhammad Ismail Shahzad <57398621+MuhammadIsmailShahzad@users.noreply.github.com>
2023-11-08 21:34:45 +0500

    Fixes compatibility with CKAN 2.10 (#2)
    * [ci.yml] Pin CKAN version
    
    * Update import for rendering templates
    
    * [templates] Use built-in jinja truncate function

--- ckanext/issues/logic/action/action.py
+++ ckanext/issues/logic/action/action.py
@@ -5,7 +5,7 @@ import ckan.logic as logic
 import ckan.plugins as p
 import ckan.model as model
 from ckan.lib import mailer
-from ckan.lib.base import render_jinja2
+from ckan.lib.base import render
 from ckan.logic import validate
 import ckan.lib.helpers as h
 import ckanext.issues.model as issuemodel
@@ -156,14 +156,14 @@ def _get_issue_email_body(issue, issue_subject, user_obj, recipient):
     extra_vars = _get_issue_vars(issue, issue_subject, user_obj, recipient)
     # Would use p.toolkit.render, but it mucks with response and other things,
     # which is unnecessary, and p.toolkit.render_text uses genshi...
-    return render_jinja2('issues/email/new_issue.html', extra_vars=extra_vars)
+    return render('issues/email/new_issue.html', extra_vars=extra_vars)
 
 
 def _get_comment_email_body(comment, issue_subject, user_obj, recipient):
     extra_vars = _get_issue_vars(comment.issue, issue_subject, user_obj,
                                  recipient)
     extra_vars['comment'] = comment
-    return render_jinja2('issues/email/new_comment.html',
+    return render('issues/email/new_comment.html',
                          extra_vars=extra_vars)
 
 
