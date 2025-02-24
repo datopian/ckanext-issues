59277c13d3deb8cbef44362bd8e70c05d5ed7865
Luccas Mateus <luccasmmg@gmail.com>
2025-02-24 12:46:54 -0300

    Fix lack of title in tests

--- ckanext/issues/tests/factories.py
+++ ckanext/issues/tests/factories.py
@@ -3,12 +3,23 @@ from pytest_factoryboy import register
 
 from ckanext.issues import model
 from ckan.tests.factories import CKANFactory
-
+import ckan.tests.helpers as helpers
 
 class Issue(CKANFactory):
     class Meta:
         model = model.Issue
         action = 'issue_create'
+
+    @classmethod
+    def api_create(cls, data_dict):
+        """Create entity via API call."""
+        title = data_dict.get('title', None)
+        if title is None:
+            title = 'Test issue'
+            data_dict['title'] = title
+        data_dict = cls._api_prepare_args(data_dict)
+        result = helpers.call_action(cls._meta.action, **data_dict)
+        return cls._api_postprocess_result(result)
 
 class IssueComment(CKANFactory):
     class Meta:
