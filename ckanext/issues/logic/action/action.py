42d86f29cca260558a3df714cab5b792c6312085
Luccas Mateus <luccasmmg@gmail.com>
2025-05-20 18:17:37 -0300

    NHS 2.11

--- ckanext/issues/logic/action/action.py
+++ ckanext/issues/logic/action/action.py
@@ -25,7 +25,6 @@ _get_or_bust = logic.get_or_bust
 
 log = logging.getLogger(__name__)
 
-
 def _add_reports(obj, can_edit, current_user):
     reports = [r.user_id for r in obj.abuse_reports]
     if can_edit:
@@ -37,6 +36,45 @@ def _add_reports(obj, can_edit, current_user):
         else:
             return []
 
+def _create_issues_activity(context, object_id, activity_type, data):
+    from ckanext.issues.logic.validators import object_id_validator, activity_type_exists
+    from ckanext.activity.logic.schema import default_create_activity_schema
+    get_validator = p.toolkit.get_validator
+    not_missing = get_validator('not_missing')
+    not_empty = get_validator('not_empty')
+    ignore_empty = get_validator('ignore_empty')
+    unicode_safe = get_validator('unicode_safe')
+
+    if context.get('ignore_activity_creation', False):
+        return
+    user = context['user']
+    user_obj = model.User.get(user)
+    activit_schema= default_create_activity_schema()
+    activit_schema.update({
+        'object_id': [not_missing, not_empty, unicode_safe, object_id_validator],
+        'activity_type': [not_missing, not_empty, unicode_safe,
+                          activity_type_exists],
+    })
+
+    activity_create_context = {
+        'model': model,
+        'user': user,
+        'ignore_auth': True,
+        'session': model.Session,
+        'schema': activit_schema
+    }
+
+    data.update({
+        'package': logic.get_action('package_show')({'model': model, 'ignore_auth': True}, {'id': object_id}),
+    })
+    
+    activity_dict = {
+        'user_id': user_obj.id,
+        'object_id': object_id,
+        'activity_type': activity_type,
+        'data': data
+    }
+    logic.get_action('activity_create')(activity_create_context, activity_dict)
 
 @p.toolkit.side_effect_free
 @validate(schema.issue_show_schema)
@@ -221,6 +259,7 @@ def issue_create(context, data_dict):
                 # TypeError occurs when we're running command from ckanapi
                 log.debug(e)
 
+    _create_issues_activity(context, dataset.id, 'new issue', issue.as_dict())
     log.debug('Created issue %s (%s)' % (issue.title, issue.id))
     return issue.as_dict()
 
@@ -264,6 +303,8 @@ def issue_update(context, data_dict):
             setattr(issue, k, v)
 
     if status_change:
+        activity_type = 'issue closed' if data_dict.get('status') == 'closed' else 'issue reopened'
+        _create_issues_activity(context, data_dict['dataset_id'], activity_type, issue.as_dict())
         if data_dict['status'] == issuemodel.ISSUE_STATUS.closed:
             issue.resolved = datetime.utcnow()
             user = context['user']
@@ -308,6 +349,7 @@ def issue_delete(context, data_dict):
         )
     session.delete(issue)
     session.commit()
+    _create_issues_activity(context, dataset_id, 'issue deleted', issue.as_dict())
 
 
 @p.toolkit.side_effect_free
@@ -488,6 +530,7 @@ def issue_comment_create(context, data_dict):
                 # TypeError occurs when we're running command from ckanapi
                 log.debug(e)
 
+    _create_issues_activity(context, data_dict['dataset_id'], 'changed issue', comment_dict)
     log.debug('Created issue comment %s' % (issue.id))
     return issue_comment.as_dict()
 
